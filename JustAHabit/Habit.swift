630f14416beb7bc2dc12296cfa3148da33512985
jaebooker <jaesonbooker@gmail.com>
2018-11-16 20:15:19 -0800

    adding stuff

---
+++ JustAHabit/Habit.swift
@@ -0,0 +1,13 @@
+//
+//  Habit.swift
+//  JustAHabit
+//
+//  Created by Jaeson Booker on 11/16/18.
+//  Copyright © 2018 Jaeson Booker. All rights reserved.
+//
+
+import Foundation
+
+struct Habit {
+    var title: String
+}
--- JustAHabit/Views/HabitsTableViewController.swift
+++ JustAHabit/Views/HabitsTableViewController.swift
@@ -11,9 +11,15 @@ import UIKit
 class HabitsTableViewController: UITableViewController {
     
     var names: [String] = ["Moony", "Wormtail", "Padfoot", "Prongs"]
-    
+    var habits: [Habit] = [
+        Habit(title: "Smoking"),
+        Habit(title: "Drinking"),
+        Habit(title: "Running over pedestrians"),
+        Habit(title: "Eating dust")
+    ]
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return names.count
+        return habits.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         var cell: UITableViewCell
@@ -24,24 +30,25 @@ class HabitsTableViewController: UITableViewController {
         }
         let name = names[indexPath.row]
         cell.textLabel?.text = name
+        let habit = habits[indexPath.row]
+        cell.textLabel?.text = habit.title
         return cell
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-//         Do any additional setup after loading the view.
+        setupNavBar()
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+extension HabitsTableViewController {
+    func setupNavBar() {
+        title = "JustAHabit"
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(pressAddHabit(_:)))
+        navigationItem.rightBarButtonItem = addButton
     }
-    */
-
+    @objc func pressAddHabit(_ sender: UIBarButtonItem) {
+        names.insert("Yo, Planet, What's Up?", at: 0)
+        let topIndexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [topIndexPath], with: .automatic)
+    }
 }

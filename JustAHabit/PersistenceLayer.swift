688ba25e013bf93bbaef390b751d5c9aeaea3ee8
jaebooker <jaesonbooker@gmail.com>
2018-11-21 03:10:33 -0800

    adding marked completion

--- JustAHabit/PersistenceLayer.swift
+++ JustAHabit/PersistenceLayer.swift
@@ -11,6 +11,14 @@ struct PersistenceLayer {
     private(set) var habits: [Habit] = []
     private static let userDefaultsHabitKeyValue = "HABITS_ARRAY"
     
+    private func saveHabits() {
+        guard let habitsData = try? JSONEncoder().encode(self.habits) else {
+            fatalError("failed to encode list of habits")
+        }
+        let userDefaults = UserDefaults.standard
+        userDefaults.set(habitsData, forKey: PersistenceLayer.userDefaultsHabitKeyValue)
+    }
+    
     private mutating func loadHabits() {
         let userDefaults = UserDefaults.standard
         guard let habitData = userDefaults.data(forKey: PersistenceLayer.userDefaultsHabitKeyValue),
@@ -19,12 +27,27 @@ struct PersistenceLayer {
         }
         self.habits = habits
     }
-    private func saveHabits() {
-        guard let habitsData = try? JSONEncoder().encode(self.habits) else {
-            fatalError("failed to encode list of habits")
+    mutating func markHabitAsCompleted(_ habitIndex: Int) -> Habit {
+        var updatedHabit = self.habits[habitIndex]
+        guard updatedHabit.hasCompletedForToday == false else { return updatedHabit }
+        updatedHabit.numberOfCompletions += 1
+        if let lastCompletionDate = updatedHabit.lastCompletionDate, lastCompletionDate.isYesterday {
+            updatedHabit.currentStreak += 1
+        } else {
+            updatedHabit.currentStreak = 1
         }
-        let userDefaults = UserDefaults.standard
-        userDefaults.set(habitsData, forKey: PersistenceLayer.userDefaultsHabitKeyValue)
+        if updatedHabit.currentStreak > updatedHabit.bestStreak {
+            updatedHabit.bestStreak = updatedHabit.currentStreak
+        }
+        let now = Date()
+        updatedHabit.lastCompletionDate = now
+        self.habits[habitIndex] = updatedHabit
+        self.saveHabits()
+        return updatedHabit
+    }
+    private mutating func deleteHabit( _habitIndex: Int) {
+        self.habits.remove(at: _habitIndex)
+        self.saveHabits() //persist changes made
     }
     init() {
         self.loadHabits()
